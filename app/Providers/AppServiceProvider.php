b43c40ea4b65ac4352872b07aacd8b64392a89d8
gafriputra <gafri.putra@gmail.com>
2023-04-29 19:09:23 +0700

    setup file manager

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // dynamic content
+        $path = 'app/public/files/shares/content.json';
+        if (file_exists(storage_path($path))) {
+            $content = json_decode(file_get_contents(storage_path($path)), true);
+            config(['content' => $content]);
+        }
         Paginator::useBootstrap();
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -23,7 +23,8 @@
                             <div class="bolder-text">
                                 {{ 'Rp ' . number_format($campaign->donations_sum_amount, 0, ',', '.') }}</div>
                             <div>Terkumpul dari
-                                <strong>{{ 'Rp ' . number_format($campaign->target_amount, 0, ',', '.') }}</strong></div>
+                                <strong>{{ 'Rp ' . number_format($campaign->target_amount, 0, ',', '.') }}</strong>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -39,8 +40,9 @@
                 <div class="col-md-6">
 
                     <div class="video-div">
-                        <a href="http://www.youtube.com/watch?v=0O2aH4XLbto" class="popup-youtube"><img
-                                src="{{ asset('html/img/video-poster.jpg') }}" alt="video-poster" class="img-fluid"></a>
+                        <a href="{{ config('content.pages.home.khotbah.youtube') }}" class="popup-youtube"><img
+                                src="{{ config('content.pages.home.khotbah.thumbnail') }}" alt="video-poster"
+                                class="img-fluid"></a>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-play-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -51,18 +53,16 @@
 
                 </div>
                 <div class="col-md-6 col-xxl-5">
-                    <div class="pre-title">Your Pre-title goes here</div>
-                    <h2>Add a Catchy Title Here</h2>
-                    <p>This space gives you more room to expound on your
-                        message. Remember that writing effectively is an art.
-                        Start by using simple, everyday words people can easily
-                        understand. Be clear and direct to the point. Keep your
-                        thoughts flowing logically, and aim for brevity.</p>
-                    <p>Your ideas have a purpose, so choose words that
-                        accurately express them. Ensure your grammar is
-                        flawless as it impacts your credibility. Use the active
-                        voice whenever possible as it makes any narrative easier
-                        to read. </p>
+                    <div class="pre-title">{{ config('content.pages.home.khotbah.sub_title') }}</div>
+                    <h2>{{ config('content.pages.home.khotbah.title') }}</h2>
+                    @php
+                        $khotbah = config('content.pages.home.khotbah.messages');
+                    @endphp
+                    @if (is_array($khotbah))
+                        @foreach ($khotbah as $item)
+                            <p>{{ $item }}</p>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
