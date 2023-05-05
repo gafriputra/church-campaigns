70ae05dec6f454c879d53328d2c9d37cf9b78d3f
gafriputra <gafri.putra@gmail.com>
2023-05-05 20:55:23 +0700

    redirect route login

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [CampaignController::class, 'index'])->name('home');
 Route::get('/donasi', [CampaignController::class, 'donasi'])->name('donasi');
 Route::get('/refresh', [CampaignController::class, 'refresh'])->name('refresh');
+Route::get('/login', function (){
+    return redirect('/admin/login');
+})->name('login');
